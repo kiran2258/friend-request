d535656f1bbd2181f1dd2747842a7219a649752c
kiran2258 <piyas2258@gmail.com>
2024-07-02 00:50:21 +0530

    javascript heart listener fix

--- Love_icon/main.js
+++ Love_icon/main.js
@@ -1,18 +1,16 @@
-let con = document.querySelector("#container")
-let love = document.querySelector("i")
+let con = document.getElementById("container");
+let love = con.querySelector("i");
 
+con.addEventListener("dblclick", function () {
+  love.style.transform = " translate(-50%,-50%) scale(1)";
+  love.style.opacity = 0.8;
+  love.style.color = "red";
 
-con.addEventListener("dblclick",function(){
-love.style.transform = " translate(-50%,-50%) scale(1);"
-love.style.opacity = 0.8
-
-
-setTimeout(function(){
-love.style.opacity = 0
-
-}, 1000)
-setTimeout(function(){
-love.style.transform = " translate(-50%,-50%) scale(0);"
-
-},2000)
-})
+  setTimeout(function () {
+    love.style.opacity = 0;
+  }, 1000);
+  setTimeout(function () {
+    love.style.transform = " translate(-50%,-50%) scale(0)";
+    love.style.color = "red";
+  }, 1000);
+});
